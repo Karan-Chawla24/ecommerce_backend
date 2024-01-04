d9ba4955276c6f620518b1fefdcb5d9fbdc3c094
KaranChawla <chawlakaran663@gmail.com>
2024-01-04 22:31:30 +0530

    update checkout route code

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,15 +1,23 @@
 import { instance } from "../server.js";
 
 export const checkout = async (req, res, next) => {
-  var options = {
-    amount: Number(req.body.amount * 100),
-    currency: "USD",
-  };
-  console.log("amount", req.body.amount);
-  const order = await instance.orders.create(options);
-  console.log(order);
-  res.status(200).json({
-    success: true,
-    order,
-  });
+  try {
+    var options = {
+      amount: Number(req.body.amount * 100),
+      currency: "USD",
+    };
+    console.log("amount", req.body.amount);
+    const order = await instance.orders.create(options);
+    console.log(order);
+    res.status(200).json({
+      success: true,
+      order,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "internal server error",
+      error: error.message,
+    });
+  }
 };
