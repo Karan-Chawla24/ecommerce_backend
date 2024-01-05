cd8e6a240370d187623ac05c16aec595cf6418bf
KaranChawla <chawlakaran663@gmail.com>
2024-01-05 19:32:23 +0530

    update headers code

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,15 +1,23 @@
 import { instance } from "../server.js";
 
 export const checkout = async (req, res, next) => {
-  var options = {
-    amount: Number(req.body.amount * 100),
-    currency: "USD",
-  };
-  console.log("amount", req.body.amount);
-  const order = await instance.orders.create(options);
-  console.log(order);
-  res.status(200).json({
-    success: true,
-    order,
-  });
+  try {
+    var options = {
+      amount: Number(req.body.amount * 100),
+      currency: "USD",
+    };
+    console.log("amount", req.body.amount);
+    const order = await instance.orders.create(options);
+    console.log(order);
+    res.status(200).json({
+      success: true,
+      order,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      error,
+    });
+  }
 };
