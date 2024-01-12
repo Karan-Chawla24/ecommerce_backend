33ecb91d8a266b3cea1406e25f7a569f05b5017d
KaranChawla <chawlakaran663@gmail.com>
2024-01-12 15:25:49 +0530

    update success page code

--- controllers/payment.js
+++ controllers/payment.js
@@ -49,12 +49,8 @@ export const paymentVerification = async (req, res, next) => {
       { $set: { products: [] } },
       { new: true }
     );
-    // res.status(200).json({
-    //   success: true,
-    //   redirectUrl: `${process.env.FRONTEND_URL}/paymentsuccess/${razorpay_payment_id}`,
-    // });
     res.redirect(
-      `${process.env.FRONTEND_URL}/paymentsuccess/${razorpay_payment_id}`
+      `http://localhost:5173/paymentsuccess?reference=${razorpay_payment_id}`
     );
   } else {
     res.status(400).json({
